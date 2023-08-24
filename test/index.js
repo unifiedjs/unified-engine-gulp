151eade535fb55f5bac596eb6a7cf0933693f490
Titus Wormer <tituswormer@gmail.com>
2023-08-24 16:20:26 +0200

    Refactor to use `node:test`

--- test/index.js
+++ test/index.js
@@ -3,11 +3,12 @@
  * @typedef {import('vinyl')} Vinyl
  */
 
+import assert from 'node:assert/strict'
 import {Buffer} from 'node:buffer'
 import {PassThrough} from 'node:stream'
+import test from 'node:test'
 import PluginError from 'plugin-error'
 import File from 'vinyl'
-import test from 'tape'
 import remarkSlug from 'remark-slug'
 import remarkHtml from 'remark-html'
 import {gulpEngine} from '../index.js'
@@ -24,12 +25,10 @@ const report = [
   ''
 ].join('\n')
 
-test('unified-engine-gulp', (t) => {
-  t.plan(9)
-
-  t.test('configuring', (st) => {
-    st.throws(
-      () => {
+test('unified-engine-gulp', async function (t) {
+  await t.test('configuring', async function () {
+    assert.throws(
+      function () {
         // @ts-expect-error: runtime.
         gulpEngine()
       },
@@ -37,149 +36,154 @@ test('unified-engine-gulp', (t) => {
       'should throw without configuration'
     )
 
-    st.throws(
-      () => {
+    assert.throws(
+      function () {
         // @ts-expect-error: runtime.
         gulpEngine({})
       },
       /^Error: Expected `name` in `configuration`$/,
       'should throw without name in configuration'
     )
-
-    st.end()
   })
 
-  t.test('null', (st) => {
-    const stderr = spy()
+  await t.test('null', async function () {
+    await new Promise(function (resolve) {
+      const stderr = spy()
 
-    st.plan(2)
-
-    example({streamError: stderr.stream})
-      .once('data', (/** @type {Vinyl} */ file) => {
-        st.equal(file.contents, null, 'should pass through nullish files')
-        st.equal(String(stderr()), '', 'should not report')
-      })
-      .write(new File({path: 'readme.md'}))
-  })
-
-  t.test('stream', (st) => {
-    const stderr = spy()
-
-    st.plan(2)
-
-    example({streamError: stderr.stream})
-      .once('error', (error) => {
-        st.ok(error instanceof PluginError, 'should pass a plugin error')
-        st.equal(error.message, 'Streaming not supported')
-      })
-      .write(new File({path: 'readme.md', contents: new PassThrough()}))
-  })
-
-  t.test('buffer', (st) => {
-    const stderr = spy()
-
-    st.plan(2)
-
-    example({streamError: stderr.stream})
-      .once('data', (/** @type {Vinyl} */ file) => {
-        st.equal(
-          String(file.contents),
-          '# h1\n\n## h2\n',
-          'should work on a buffer'
-        )
-
-        st.equal(String(stderr()), report, 'should report')
-      })
-      .write(new File({path: 'readme.md', contents: Buffer.from(input)}))
-  })
-
-  t.test('ignore files', (st) => {
-    const stderr = spy()
-
-    st.plan(2)
-
-    example({streamError: stderr.stream})
-      .once('data', (/** @type {Vinyl} */ file) => {
-        st.equal(String(file.contents), input, 'should not mutate buffer')
-        st.equal(String(stderr()), '', 'should not report')
-      })
-      .write(new File({path: 'ignored.md', contents: Buffer.from(input)}))
-  })
-
-  t.test('frail mode', (st) => {
-    const stderr = spy()
-
-    st.plan(3)
-
-    example({frail: true, streamError: stderr.stream})
-      .once('error', (error) => {
-        st.ok(error instanceof PluginError, 'should pass a plugin error')
-        st.equal(error.message, 'Unsuccessful running')
-        st.equal(String(stderr()), report, 'should report')
-      })
-      .write(new File({path: 'readme.md', contents: Buffer.from(input)}))
-  })
-
-  t.test('error handling', (st) => {
-    const stderr = spy()
-
-    st.plan(1)
-
-    example({filePath: '!', streamError: stderr.stream})
-      .once('error', (error) => {
-        st.ok(
-          error.message.startsWith(
-            'Do not pass both `filePath` and real files'
-          ),
-          'should pass fatal errors'
-        )
-      })
-      .write(new File({path: 'readme.md', contents: Buffer.from(input)}))
-  })
-
-  t.test('using plugins', (st) => {
-    const stderr = spy()
-
-    st.plan(2)
-
-    example({streamError: stderr.stream})
-      .use(remarkSlug)
-      .use(remarkHtml)
-      .once('data', (/** @type {Vinyl} */ file) => {
-        st.equal(
-          String(file.contents),
-          '<h1 id="user-content-h1">h1</h1>\n<h2 id="user-content-h2">h2</h2>\n',
-          'should work with a plug-in'
-        )
-
-        st.equal(String(stderr()), report, 'should report')
-      })
-      .write(new File({path: 'readme.md', contents: Buffer.from(input)}))
-  })
-
-  t.test('custom data', (st) => {
-    const stderr = spy()
-
-    st.plan(1)
-
-    example({streamError: stderr.stream})
-      .use(() => {
-        /** @type {Transformer} */
-        return function (_, file) {
-          file.data.value = 'changed'
-        }
-      })
-      .once('data', (/** @type {Vinyl} */ file) => {
-        // Coverage.
-        // type-coverage:ignore-next-line
-        st.equal(file.data.value, 'changed')
-      })
-      .write(
-        new File({
-          path: 'readme.md',
-          contents: Buffer.from(input),
-          data: {value: 'original'}
+      example({streamError: stderr.stream})
+        .once('data', function (/** @type {Vinyl} */ file) {
+          assert.equal(file.contents, null, 'should pass through nullish files')
+          assert.equal(String(stderr()), '', 'should not report')
+          resolve(undefined)
         })
-      )
+        .write(new File({path: 'readme.md'}))
+    })
+  })
+
+  await t.test('stream', async function () {
+    await new Promise(function (resolve) {
+      const stderr = spy()
+
+      example({streamError: stderr.stream})
+        .once('error', function (error) {
+          assert.ok(error instanceof PluginError, 'should pass a plugin error')
+          assert.equal(error.message, 'Streaming not supported')
+          resolve(undefined)
+        })
+        .write(new File({path: 'readme.md', contents: new PassThrough()}))
+    })
+  })
+
+  await t.test('buffer', async function () {
+    await new Promise(function (resolve) {
+      const stderr = spy()
+
+      example({streamError: stderr.stream})
+        .once('data', function (/** @type {Vinyl} */ file) {
+          assert.equal(
+            String(file.contents),
+            '# h1\n\n## h2\n',
+            'should work on a buffer'
+          )
+          assert.equal(String(stderr()), report, 'should report')
+          resolve(undefined)
+        })
+        .write(new File({path: 'readme.md', contents: Buffer.from(input)}))
+    })
+  })
+
+  await t.test('ignore files', async function () {
+    await new Promise(function (resolve) {
+      const stderr = spy()
+
+      example({streamError: stderr.stream})
+        .once('data', function (/** @type {Vinyl} */ file) {
+          assert.equal(String(file.contents), input, 'should not mutate buffer')
+          assert.equal(String(stderr()), '', 'should not report')
+          resolve(undefined)
+        })
+        .write(new File({path: 'ignored.md', contents: Buffer.from(input)}))
+    })
+  })
+
+  await t.test('frail mode', async function () {
+    await new Promise(function (resolve) {
+      const stderr = spy()
+
+      example({frail: true, streamError: stderr.stream})
+        .once('error', function (error) {
+          assert.ok(error instanceof PluginError, 'should pass a plugin error')
+          assert.equal(error.message, 'Unsuccessful running')
+          assert.equal(String(stderr()), report, 'should report')
+          resolve(undefined)
+        })
+        .write(new File({path: 'readme.md', contents: Buffer.from(input)}))
+    })
+  })
+
+  await t.test('error handling', async function () {
+    await new Promise(function (resolve) {
+      const stderr = spy()
+
+      example({filePath: '!', streamError: stderr.stream})
+        .once('error', function (error) {
+          assert.ok(
+            error.message.startsWith(
+              'Do not pass both `filePath` and real files'
+            ),
+            'should pass fatal errors'
+          )
+          resolve(undefined)
+        })
+        .write(new File({path: 'readme.md', contents: Buffer.from(input)}))
+    })
+  })
+
+  await t.test('using plugins', async function () {
+    await new Promise(function (resolve) {
+      const stderr = spy()
+
+      example({streamError: stderr.stream})
+        .use(remarkSlug)
+        .use(remarkHtml)
+        .once('data', function (/** @type {Vinyl} */ file) {
+          assert.equal(
+            String(file.contents),
+            '<h1 id="user-content-h1">h1</h1>\n<h2 id="user-content-h2">h2</h2>\n',
+            'should work with a plug-in'
+          )
+
+          assert.equal(String(stderr()), report, 'should report')
+          resolve(undefined)
+        })
+        .write(new File({path: 'readme.md', contents: Buffer.from(input)}))
+    })
+  })
+
+  await t.test('custom data', async function () {
+    await new Promise(function (resolve) {
+      const stderr = spy()
+
+      example({streamError: stderr.stream})
+        .use(function () {
+          /** @type {Transformer} */
+          return function (_, file) {
+            file.data.value = 'changed'
+          }
+        })
+        .once('data', function (/** @type {Vinyl} */ file) {
+          // Coverage.
+          // type-coverage:ignore-next-line
+          assert.equal(file.data.value, 'changed')
+          resolve(undefined)
+        })
+        .write(
+          new File({
+            path: 'readme.md',
+            contents: Buffer.from(input),
+            data: {value: 'original'}
+          })
+        )
+    })
   })
 })
